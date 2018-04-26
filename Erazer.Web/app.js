4e54b73fe41c4caa7f03e680879efec61c5abc2d
ErazerBrecht <erazerbrecht@gmail.com>
2018-04-26 19:52:51 +0200

    Updated nodejs app to support AngularSSR + Redis store (session) + Compression + ...

--- Erazer.Web/app.js
+++ Erazer.Web/app.js
@@ -1,56 +1,79 @@
+'use strict';
+
+require('zone.js/dist/zone-node');
+
 const express = require('express');
 const app = express();
 
-const flash = require('connect-flash');
+const ngUniversal = require('@nguniversal/express-engine');
+const appServer = require('./wwwroot/landing-server/main.bundle');
+
+const compression = require('compression');
 const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
+const mime = require('mime/lite');
 const session = require('express-session');
+const redisStore = require('connect-redis')(session);
 const passport = require('passport');
 const { Issuer, Strategy } = require('openid-client');
 const url = require('url');
 
+const wwwroot = __dirname + '/wwwroot';
+
 module.exports = new Promise(async (resolve, reject) => {
     app.use(morgan('dev'));
-    app.use(cookieParser());
+    app.use(compression());
 
-    app.use(express.static(__dirname + '/public'));
+    app.use(session({
+        store: new redisStore({ host: 'localhost', port: '6380' }),
+        secret: 'tutorialsecret',
+        resave: false,
+        saveUninitialized: false
+    }));
 
-    app.use(session({ secret: 'tutorialsecret', resave: false, saveUninitialized: false }));
     app.use(passport.initialize());
     app.use(passport.session());
 
-    app.use(flash());
+    app.use(redirectMiddleware);
+    app.get('/', angularRouter);
+    app.get('/profile', angularRouter);
+
+    app.use(express.static(wwwroot, {
+        setHeaders: function (res, path) {
+            if (mime.getType(path) === 'text/html') {
+                res.setHeader('Cache-Control', 'public, max-age=0');
+            }
+            else {
+                res.setHeader('Cache-Control', 'public, max-age=31536000, immutable');
+            }
+        }
+    }));
+
+    /* Configure Angular Express engine */
+    app.engine('html', ngUniversal.ngExpressEngine({
+        bootstrap: appServer.AppServerModuleNgFactory
+    }));
+    app.set('view engine', 'html');
+    app.set('views', 'wwwroot');
 
     try {
-        const oidc = await addOidc();
-        addAuth(oidc);
+        addAuth(await addOidc());
     }
     catch (error) {
         reject(error);
     }
-
-
-    // TODO Routing
-    app.get('/', (req, res) => {
-        console.log(req.user);
-        if (req.user)
-            res.send(`Hello ${req.user.name}`);
-        else
-            res.send('Hello World')
-    });
 
     resolve(app);
 });
 
 
 async function addOidc() {
-    const erazerSSO = await Issuer.discover('http://localhost:5000');
-    const client = new erazerSSO.Client({ client_id: 'nodejs', client_secret: 'C1C47B06-7E3B-41D6-BB2D-F4DF245DBF7C' });
+    const issuer = await Issuer.discover('http://localhost:5000');
+    const client = new issuer.Client({ client_id: 'nodejs', client_secret: 'C1C47B06-7E3B-41D6-BB2D-F4DF245DBF7C' });
 
-    return {issuer: erazerSSO, client: client};
+    return { issuer, client };
 }
 
-function addAuth(oidc){
+function addAuth(oidc) {
     const params = {
         redirect_uri: 'http://localhost:8888/signin-oidc',
         scope: 'openid profile'
@@ -78,18 +101,43 @@ function addAuth(oidc){
 
     app.get('/logout', (req, res) => {
         const idToken = req.user.id_token;
-        
-        req.logout(); 
+
+        req.logout();
         res.redirect(url.format(Object.assign(url.parse(oidc.issuer.end_session_endpoint), {
             search: null,
             query: {
-              id_token_hint: idToken,
-              post_logout_redirect_uri: 'http://localhost:8888'
+                id_token_hint: idToken,
+                post_logout_redirect_uri: 'http://localhost:8888'
             },
-      })));
+        })));
     });
 
-    app.get('/auth', passport.authenticate('oidc'));
-    app.get('/signin-oidc', passport.authenticate('oidc', { successRedirect: '/', failureRedirect: '/login' }));
+    app.get('/dashboard', passport.authenticate('oidc'));
+    app.get('/signin-oidc', passport.authenticate('oidc', { successRedirect: '/portal', failureRedirect: '/' }));
+    
+    app.get('/token', (req, res) => {
+        if (!req.user) {
+            res.send(401);
+        }
+        res.json(req.user.access_token);
+    });
+}
 
+function redirectMiddleware(req, res, next) {;
+    if (req.url.startsWith('/portal'))
+    {
+        if (!req.user)
+            return res.redirect('/');
+        if (!req.url.includes('.'))
+            return res.sendFile('portal/index.html', {root: wwwroot});
+    }
+    else if (req.url === '/' && req.user)
+        return res.redirect('/portal');
+
+    next();
+}
+
+function angularRouter(req, res) {
+    /* Server-side rendering */
+    res.render('index', { req, res });
 }
--- Erazer.Web/index.js
+++ Erazer.Web/index.js
@@ -1,10 +1,28 @@
+const fs = require('fs');
+const spdy = require('spdy');
 const appPromise = require('./app');
 
+const options = {
+	key: fs.readFileSync(__dirname + '/server.key'),
+	cert: fs.readFileSync(__dirname + '/server.crt')
+}
+
 appPromise.then(app => {
+	const port = process.env.PORT || 8888;
+	
+	// HTTP 1
 	app.set('port', (process.env.PORT || 8888));
 	app.listen(app.get('port'), () => {
 		console.log('Node app is running on port', app.get('port'));
 	});
+	
+	// HTTP2
+	// spdy.createServer(options, app).listen(port, (error) => {
+	// 	console.log('Node app is running on port', port);
+	// });
+}, (error) => {
+	console.error('Something went wrong');
+	console.error(error);
 });
 
 
