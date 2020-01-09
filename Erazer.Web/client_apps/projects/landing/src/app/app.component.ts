23c51782a419c135317506aa9ff70a223ecde350
Brecht Carlier <erazerbrecht@gmail.com>
2020-01-09 19:49:34 +0100

    Improvements for Angular Universal

--- Erazer.Web/client_apps/projects/landing/src/app/app.component.ts
+++ Erazer.Web/client_apps/projects/landing/src/app/app.component.ts
@@ -1,27 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    styleUrls: ['./app.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
-    //@ViewChild(NavbarComponent) navbar: NavbarComponent;
-
     constructor() { }
 
     ngOnInit() {
-        // var navbar: HTMLElement = this.element.nativeElement.children[0].children[0];
-
-        // this.renderer.listenGlobal('window', 'scroll', (event) => {
-        //     const number = window.scrollY;
-        //     if (number > 150 || window.pageYOffset > 150) {
-        //         // add logic
-        //         navbar.classList.remove('navbar-transparent');
-        //     } else {
-        //         // remove logic
-        //         navbar.classList.add('navbar-transparent');
-        //     }
-        // });
     }
 }
