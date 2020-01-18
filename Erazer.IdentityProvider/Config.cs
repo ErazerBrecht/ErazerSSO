cf7c1f5f2660f4701ad9ac8f95dce479ea90250b
ErazerBrecht <brecht.carlier@student.ap.be>
2020-01-18 13:39:01 +0100

    Renamed Erazer.Authorization to Erazer.IdentityProvider

---
+++ Erazer.IdentityProvider/Config.cs
@@ -0,0 +1,106 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using System;
+using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace Erazer.IdentityProvider
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new ApiResource[]
+            {
+                new ApiResource("api1", "My API #1")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients(string hostname)
+        {
+            return new[]
+            {
+                // Legacy client (MVC + AngularJS 1.X) using hybrid flow
+                new Client
+                {
+                    ClientId = "legacy",
+                    ClientName = "ErazerSSO Legacy",
+
+                    RequireConsent = false,
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+                    RedirectUris = { $"{hostname}:5001/signin-oidc" },
+                    FrontChannelLogoutUri = $"{hostname}:5001/signout-oidc",
+                    PostLogoutRedirectUris = { $"{hostname}:5001/signout-callback-oidc" },
+
+                    AccessTokenLifetime = 1800,
+                    AbsoluteRefreshTokenLifetime = (int) TimeSpan.FromHours(8).TotalSeconds,
+                    RefreshTokenExpiration = TokenExpiration.Absolute,
+                    RefreshTokenUsage = TokenUsage.ReUse,
+                    AllowOfflineAccess = true,
+                    AllowedScopes = { "openid", "profile", "api1" }
+                },
+
+                // New client (NodeJS + Angular) using 'code authorization'
+                new Client
+                {
+                    ClientId = "nodejs",
+                    ClientName = "ErazerSSO nodejs",
+                    
+                    AccessTokenLifetime = 3600,
+                    AllowOfflineAccess = false,
+
+                    RequireConsent = false,
+                    AllowedGrantTypes = GrantTypes.Code,
+                    ClientSecrets = { new Secret("C1C47B06-7E3B-41D6-BB2D-F4DF245DBF7C".Sha256()) },
+
+                    RedirectUris = { $"{hostname}:8888/auth/signin-oidc" },
+                    PostLogoutRedirectUris = { $"{hostname}:8888" },
+
+                    AllowedScopes = { "openid", "profile", "api1" }
+                },
+                new Client
+                {
+                    ClientId = "nodejs_dev",
+                    ClientName = "ErazerSSO nodejs_dev",
+
+                    AccessTokenLifetime = (int) TimeSpan.FromDays(7).TotalSeconds,
+                    AllowOfflineAccess = false,
+
+                    RequireConsent = false,
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    ClientSecrets = { new Secret("425A4639-4079-49E1-9F86-E832F246F5FB".Sha256()) },
+
+                    AllowedScopes = { "openid", "profile", "api1" }
+                },
+                new Client
+                {
+                    ClientId = "thirdparty",
+                    ClientName = "Thirdparty",
+
+                    RequireConsent = true,
+                    AllowedGrantTypes = GrantTypes.Code,
+                    ClientSecrets = { new Secret("EC095F67-66AB-40E5-A140-6E4806194CD9".Sha256()) },
+
+                    RedirectUris = { $"{hostname}:9999/signin-oidc" },
+                    FrontChannelLogoutUri = $"{hostname}:9999/signout-oidc",
+                    PostLogoutRedirectUris = { $"{hostname}:9999/signout-callback-oidc" },
+
+                    AllowedScopes = { "openid", "profile" }
+                },
+            };
+        }
+    }
+}
---
+++ Erazer.IdentityProvider/Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Reflection;
+using IdentityServer4.Quickstart.UI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Erazer.IdentityProvider
+{
+    public class Startup
+    {
+        private IHostingEnvironment Environment { get; }
+        private IConfiguration Configuration { get; }
+
+        public Startup(IHostingEnvironment environment, IConfiguration configuration)
+        {
+            Environment = environment ?? throw new ArgumentNullException(nameof(environment));
+            Configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var hostname = Configuration["baseUrl"];
+            services.AddMvc();
+
+            var builder = services.AddIdentityServer(options =>
+            {
+                options.Authentication.CookieLifetime = TimeSpan.FromHours(12);
+                options.Authentication.CookieSlidingExpiration = true;
+                options.Authentication.CheckSessionCookieName = "Erazer.SSO.Idsr";
+
+                // TODO Check if I don't have to disable them in non-dev environment
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+            });
+             
+            builder.AddInMemoryIdentityResources(Config.GetIdentityResources());
+            builder.AddInMemoryApiResources(Config.GetApis());
+            builder.AddInMemoryClients(Config.GetClients(hostname));
+            builder.AddTestUsers(TestUsers.Users);
+            builder.AddInMemoryPersistedGrants();
+
+            if (Environment.IsDevelopment())
+            {
+                builder.AddDeveloperSigningCredential();
+            }
+            else
+            {
+                // TODO DON'T DO THIS IN PRODUCTION!!!!!!!
+                builder.AddDeveloperSigningCredential();
+            }
+
+            services.AddAuthentication();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseCors(c => c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
