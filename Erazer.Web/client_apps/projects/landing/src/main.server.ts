a7efbf07feac10533b2d7b27b72c9d6ba7416dcd
ErazerBrecht <brecht.carlier@student.ap.be>
2020-03-08 12:48:38 +0100

    Fixed SSR with Ivy

--- Erazer.Web/client_apps/projects/landing/src/main.server.ts
+++ Erazer.Web/client_apps/projects/landing/src/main.server.ts
@@ -1,8 +1,6 @@
-/***************************************************************************************************
- * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
- */
 import '@angular/localize/init';
 import { enableProdMode } from '@angular/core';
+
 export { AppServerModule } from './app/app-server.module';
 export { ngExpressEngine } from '@nguniversal/express-engine';
 export { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
--- Erazer.Web/ssr.js
+++ Erazer.Web/ssr.js
@@ -1,16 +1,12 @@
 require('zone.js/dist/zone-node');
-const { AppServerModuleNgFactory, LAZY_MODULE_MAP, ngExpressEngine, provideModuleMap } = require('./wwwroot/landing-ssr/main');
+const { AppServerModule, ngExpressEngine } = require('./wwwroot/landing-ssr/main');
 
 
 module.exports = (app) => {
     const wwwroot = app.get('config').wwwroot;
-
     /* Configure Angular Express engine */
     app.engine('html', ngExpressEngine({
-        bootstrap: AppServerModuleNgFactory,
-        providers: [
-            provideModuleMap(LAZY_MODULE_MAP)
-        ]
+        bootstrap: AppServerModule,
     }));
     app.set('view engine', 'html');
     app.set('views', wwwroot);
