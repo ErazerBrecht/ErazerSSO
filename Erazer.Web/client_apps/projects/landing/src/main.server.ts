299611bf1906b4c3b11dfe08742a56a3b7a38ef4
ErazerBrecht <brecht.carlier@student.ap.be>
2020-03-24 00:40:39 +0100

    Update ng universal to 9.0.2

--- Erazer.Web/client_apps/projects/landing/src/main.server.ts
+++ Erazer.Web/client_apps/projects/landing/src/main.server.ts
@@ -3,7 +3,6 @@ import { enableProdMode } from '@angular/core';
 
 export { AppServerModule } from './app/app-server.module';
 export { ngExpressEngine } from '@nguniversal/express-engine';
-export { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 export { renderModule, renderModuleFactory } from '@angular/platform-server';
 
 enableProdMode();
