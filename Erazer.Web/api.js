cb9d5cb3c391b1baecb41d24c9ff6d391c7c171a
ErazerBrecht <brecht.carlier@student.ap.be>
2020-08-24 21:18:53 +0200

    Refactored authentication branches into "positive checks"

--- Erazer.Web/api.js
+++ Erazer.Web/api.js
@@ -3,79 +3,88 @@ const crypto = require('@trust/webcrypto');
 
 module.exports = (apiUrl) => {
     return async (req, res, next) => {
-        if (!req.url.startsWith('/public')) {
-            if (!req.user) {
-                return res.status(401).json({ error: 'Not signed in' });
-            }
-
-            if (req.user.isLocal !== true) {
-                if (req.user.ip !== req.ip || req.user.userAgent !== req.get('User-Agent')) {
-                    return res.status(401).json({ error: 'Incorrect session parameters' });
+        const authenticated = await authenticationCheck(req);
+        if (authenticated) {
+            return proxy(apiUrl, {
+                proxyReqOptDecorator: function (proxyReqOpts, srcReq) {
+                    if (req.user) {
+                        proxyReqOpts.headers = { "Authorization": `Bearer ${req.user.access_token}` };
+                    }
+                    return proxyReqOpts;
                 }
+            })(req, res, next);
+        } else {
+            return res.status(401).json({ error: 'Authentication failed' });
+        }
+    }
+}
 
-                const publicKey = req.user.publicKey;
-                const epoch = req.headers["x-epoch"];
-                const signature = req.headers["x-signature"];
+async function authenticationCheck(req) {
+    if (req.url.startsWith('/public')) {
+        // Public API calls don't need the auth checks
+        return true;
+    }
 
-                // Argument check
-                if (!publicKey || !epoch || !signature) {
-                    return res.status(401).json({ error: 'Signing check failed' });
-                }
+    if (req.user) {
+        if (req.user.isLocal === true) {
+            // DEVELOPMENT MODE
+            // Don't bother checking signature
+            return true;
+        }
+
+        // IP + User Agent check
+        if (req.user.ip === req.ip && req.user.userAgent === req.get('User-Agent')) {
+            const publicKey = req.user.publicKey;
+            const epoch = req.headers["x-epoch"];
+            const signature = req.headers["x-signature"];
+
+            // Argument check
+            if (publicKey && epoch && signature) {
 
                 // Epoch check
                 const epochNumber = +epoch;
                 const now = Date.now();
-                if (epochNumber < now - 10000 || epochNumber > now + 10000) {
-                    return res.status(401).json({ error: 'Signing check failed' });
-                }
+                if (epochNumber >= now - 5000 && epochNumber <= now + 5000) {
 
-                // Sign check
-                try {
-                    const key = await crypto.subtle.importKey(
-                        "jwk",
-                        publicKey,
-                        {
-                            name: "RSA-PSS",
-                            modulusLength: 4096,
-                            publicExponent: new Uint8Array([1, 0, 1]),
-                            hash: "SHA-256"
-                        },
-                        false,
-                        ["verify"]);
+                    // Sign check
+                    try {
+                        const key = await crypto.subtle.importKey(
+                            "jwk",
+                            publicKey,
+                            {
+                                name: "RSA-PSS",
+                                modulusLength: 4096,
+                                publicExponent: new Uint8Array([1, 0, 1]),
+                                hash: "SHA-256"
+                            },
+                            false,
+                            ["verify"]);
 
-                    const decodedSignature = new Uint8Array(Buffer.from(signature, 'base64'));
-                    const plain = epoch + req.originalUrl.toLowerCase();
-                    const encoded = new TextEncoder().encode(plain);
-                    const result = await crypto.subtle.verify(
-                        {
-                            name: "RSA-PSS",
-                            saltLength: 32,
-                            hash: {
-                                name: "SHA-256"
-                            }
-                        },
-                        key,
-                        decodedSignature,
-                        encoded
-                    );
+                        const decodedSignature = new Uint8Array(Buffer.from(signature, 'base64'));
+                        const plain = epoch + req.originalUrl.toLowerCase();
+                        const encoded = new TextEncoder().encode(plain);
+                        const result = await crypto.subtle.verify(
+                            {
+                                name: "RSA-PSS",
+                                saltLength: 32,
+                                hash: {
+                                    name: "SHA-256"
+                                }
+                            },
+                            key,
+                            decodedSignature,
+                            encoded
+                        );
 
-                    if (!result) {
-                        return res.status(401).json({ error: 'Signing check failed' });
+                        return result;
                     }
-                }
-                catch (e) {
-                    return res.status(401).json({ error: 'Signing check failed' });
+                    catch (e) {
+                        console.log("TODO Log something usefull");
+                    }
                 }
             }
         }
-
-        return proxy(apiUrl, {
-            proxyReqOptDecorator: function (proxyReqOpts, srcReq) {
-                if (req.user) {
-                    proxyReqOpts.headers = { "Authorization": `Bearer ${req.user.access_token}` };
-                }
-                return proxyReqOpts;
-            }
-        })(req, res, next);
     }
+
+    return false;
 }
